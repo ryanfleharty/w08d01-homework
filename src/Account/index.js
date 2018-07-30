67d2a7893e16b378e7c91c1cf05c0e65ce155b63
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-07-30 17:02:35 -0600

    step 4 complete

--- src/Account/index.js
+++ src/Account/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-
+// why does this index not need to import app.js?
 
 class Account extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      balance: 0 
+    }
+  }
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
+        <h2>TODO: {this.props.name}</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
         <input type="button" value="Deposit" />
         <input type="button" value="Withdraw" />
       </div>
