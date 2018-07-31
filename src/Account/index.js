e161807f461f10f3fcb9df0374f8f4e6f1690546
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-07-30 21:44:19 -0600

    finished but errors

--- src/Account/index.js
+++ src/Account/index.js
@@ -7,19 +7,41 @@ class Account extends Component {
     this.state = {
       balance: 0 
     }
+    handleDepositClick(e) {
+    e.preventDefault();
+    const amount = parseInt(this.inputBox.value);
+    const newBalance = this.state.balance + amount;
+    this.setState({
+      balance: newBalance
+    })
+    this.inputBox.value = "";
   }
-
+    handleWithdrawClick(e) {
+    e.preventDefault();
+    const amount = parseInt(this.inputBox.value);
+    const newBalance = this.state.balance - amount;
+    this.setState({
+      balance: newBalance
+    })
+    this.inputBox.value = "";
+  }
+  }
   render() {
+    const balanceClass = 'balance';
+    if (this.state.balance === 0){
+      balanceClass =+ ' zero';
+    }
     return (
       <div className="account">
         <h2>TODO: {this.props.name}</h2>
-        <div className="balance">$0</div>
+        <div className={balanceClass}>$0</div>
         <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
       </div>
     )
   }
+
 }
 
 export default Account;
