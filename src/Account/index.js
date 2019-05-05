f1052248dacf8280261f517b941a76dff15e6f8c
Jake Shelton <jakeshelton@Archies-Howl.local>
2019-05-05 11:17:11 -0600

    all but bonus section done

--- src/Account/index.js
+++ src/Account/index.js
@@ -43,7 +43,7 @@ class Account extends Component {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">${this.state.balance}</div>
+        <div className={balanceClass}>${this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
         <input type="button" value="Deposit" onClick={this.handleDeposit} />
         <input type="button" value="Withdraw" onClick={this.handleWithdraw} />
