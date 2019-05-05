6fb6f2847db405b1657b165061585383d85b20a8
Destiny <dshillis1@gmail.com>
2019-05-04 22:29:21 -0600

    Steps 1-4

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      balance: 0,
+    }
+  }
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
+        <h2>{ this.props.name }</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
+        <input type="text" placeholder="enter an amount" id="text-input" ref={()=>{
+          document.getElementById('text-input').value;
+        }} />
+        <input type="button" value="Deposit" onClick={()=>{
+          console.log(this.state.balance + ref)
+        }} />
         <input type="button" value="Withdraw" />
       </div>
     )
