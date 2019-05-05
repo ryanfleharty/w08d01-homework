1b292e9bf19e0c1657fb3a239db3c7c26216b72e
Destiny <dshillis1@gmail.com>
2019-05-05 14:23:43 -0600

    Deposit working

--- src/Account/index.js
+++ src/Account/index.js
@@ -12,12 +12,17 @@ class Account extends Component {
     return (
       <div className="account">
         <h2>{ this.props.name }</h2>
-        <div className="balance">$0</div>
+        <div className="balance">${ this.state.balance }</div>
             <input type="text" placeholder="enter an amount" ref={(input) => {
               this.inputBox = input
             }} />
             <input type="button" value="Deposit" onClick={() => {
+              this.setState({
+                balance: parseInt(this.state.balance) + parseInt(this.inputBox.value)
+              })
+              console.log(this.state.balance)
               console.log(this.inputBox.value)
+
             }} />
             <input type="button" value="Withdraw" onClick={() => {
               console.log(this.inputBox.value)
