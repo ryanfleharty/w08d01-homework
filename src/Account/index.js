f01eb83d1e0a0271c9f5ff2bee13b8c4823fb279
WillyD <sopwill3@gmail.com>
2019-05-05 10:58:00 -0600

    finished basic stuff

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,64 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      balance: 0,
+      value: '',
+      zeroClass: ''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+      })
+  }
+
+  handleTransaction = (e) => {
+    e.preventDefault();
+    let numberVal = parseInt(this.state.value);
+    if (numberVal) {
+      if (e.target.value === "Deposit") {
+        this.setState({
+          balance: this.state.balance + numberVal,
+          value: ''
+        })
+      } else {
+        if (this.state.balance >= numberVal){
+        this.setState({
+          balance: this.state.balance - numberVal,
+          value: ''
+        })
+      } else {
+        console.log("OVERDRAW WARNING")
+      }
+      }
+    } else {
+      console.log("please enter only numbers")
+    }
+  }
+
   render() {
+    if (this.state.balance == 0){
+      this.state.zeroClass = 'zero'
+    } else {
+      this.state.zeroClass = '';
+    }
     return (
-      <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+      <div className={"account " + this.state.zeroClass}>
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+
+        <form>
+        <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="enter an amount" />
+
+        <button type="button" onClick={this.handleTransaction} value="Deposit">Deposit</button>
+
+        <button type="button" onClick={this.handleTransaction} value="Withdraw">Withdraw</button>
+
+        </form>
+
       </div>
     )
   }
