6fabe828e6e033baaa4e6d37d0c0bec303078023
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-05-06 08:23:20 -0600

    second commit

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,17 +2,44 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      balance:0,
+    }
+  }
   render() {
+    let balanceClass = 'balance'
+    if (this.state.balance === 0) {
+      balanceClass += ' zero';
+    };
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <div className={balanceClass}>{this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(input)=>this.textInput = input}/>
+        <input type="button" value="Deposit" onClick={this.handleDepositClick}/>
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick}/>
       </div>
     )
+  };
+  handleDepositClick = (e)=> {
+    e.preventDefault();
+    const addedInput = parseInt(this.textInput.value, 16)
+    const newBalance = this.state.balance + addedInput;
+    this.setState({
+      balance:newBalance,
+    })
   }
+  handleWithdrawClick = (e) => {
+    e.preventDefault();
+    const addedInput = parseInt(this.textInput.value, 16)
+    const newBalance = this.state.balance - addedInput;
+    this.setState({
+      balance: newBalance,
+    })
+  }
+  
 }
 
 export default Account;
