ef03a9dc4ec6977333bbe800b2e30ac81cfc38d5
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-05-06 17:37:46 -0600

    fixed edgecases

--- src/Account/index.js
+++ src/Account/index.js
@@ -19,7 +19,7 @@ class Account extends Component {
 
         <h2>{this.props.name}</h2>
         <div className={balanceClass}>{this.state.balance}</div>
-        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
+        <input type="number" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
         <input type="button" value="Deposit" onClick={this.handleDepositClick}/>
         <input type="button" value="Withdraw" onClick={this.handleWithdrawlClick}/>
 
@@ -29,19 +29,28 @@ class Account extends Component {
   handleDepositClick = (e) => {
       e.preventDefault();
       const amount = parseInt(this.inputBox.value)
-      const newBalance = this.state.balance + amount
+      let newBalance = 0
+        if (this.state.balance - amount <= 0) {
+        newBalance = this.state.balance - amount;
+        } else {
+        newBalance = this.state.balance;
+      }
       this.setState({
         balance: newBalance
       })
       this.inputBox.value = ''
     }
     handleWithdrawlClick = (e) => {
+    let newBalance = 0
+      if (this.state.balance - amount >= 0) {
+      newBalance = this.state.balance - amount;
+      } else {
+      newBalance = this.state.balance;
+    }
       e.preventDefault()
       const amount = parseInt(this.inputBox.value)
-      const newBalance = this.state.balance - amount
       this.setState({
         balance: newBalance
-
       })
       this.inputBox.value = ''
     }
