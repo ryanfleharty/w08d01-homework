22ba8843fcc6b5d850dec965285feba34c5b598d
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-05-06 18:52:56 -0600

    withdrawl and deposit working

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,48 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+          balance: 0
+          
+    }
+    console.log(this.state)
+  }
+
+  handleDepositClick = (e) =>{ 
+    e.preventDefault();
+      const amount = parseInt(this.inputBox.value);
+      const newBalance = this.state.balance + amount;
+
+      this.setState ({
+        balance: newBalance
+    })
+      this.inputBox.value = " ";
+  }
+  handleWithdrawClick = (e) => {
+    e.preventDefault();
+    const amount = parseInt(this.inputBox.value);
+    const newBalance = this.state.balance - amount; 
+
+    this.setState ({
+      balance: newBalance
+    })
+      this.inputBox.value = " ";
+  }
+
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        
+        <div className="balance">{this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(input) => 
+          this.inputBox = input
+        }/>
+       
+        <input type="button" value="Deposit" onClick={this.handleDepositClick}/>
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick}/>
       </div>
     )
   }
