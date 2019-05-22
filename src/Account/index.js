356a575b2a3969d513e4f073ff8d6dd10c5c1591
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-05-22 16:08:38 -0600

    Will no longer go negative and turns red at 0

--- src/Account/index.js
+++ src/Account/index.js
@@ -21,21 +21,29 @@ class Account extends Component {
     })
       this.inputBox.value = " ";
   }
+     
+      
+
   handleWithdrawClick = (e) => {
     e.preventDefault();
+   
     const amount = parseInt(this.inputBox.value);
-    const newBalance = this.state.balance - amount; 
+     let newBalance = 0;
 
-    this.setState ({
-      balance: newBalance
-    })
-      this.inputBox.value = " ";
-  }
-
-  render() {
-    let balanceClass = 'balance';
+      if (this.state.balance - amount >= 0) {       // will not let you withdraw more than you have now
+          newBalance = this.state.balance - amount;   
+      } else {
+          newBalance = this.state.balance 
+      }
+      this.setState({ 
+        balance: newBalance
+      })
+  this.inputBox.value = '';
+    }
+    render() {
+    let  balanceClass = 'balance';
         if(this.state.balance === 0 ){
-        balanceClass += 'zero';
+        balanceClass += ' zero';
     }
 
      
@@ -44,7 +52,7 @@ class Account extends Component {
       <div className="account">
         <h2>{this.props.name}</h2>
         
-        <div className="balanceClass">{this.state.balance}</div>
+        <div className={balanceClass}>{this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref={(input) => 
           this.inputBox = input
         }/>
@@ -53,8 +61,8 @@ class Account extends Component {
         <input type="button" value="Deposit" onClick={this.handleDepositClick}/>
         <input type="button" value="Withdraw" onClick={this.handleWithdrawClick}/>
       </div>
-    )
+      )
+    }
   }
-}
 
 export default Account;
